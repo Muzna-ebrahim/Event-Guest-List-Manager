41c04fa01bd9b3ac05e95a0ec61806a40d88810b
muzna <ebrahimmuznah98@gmail.com>
2025-06-17 19:33:18 +0300

    updated version of the html,javscript

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const form = document.getElementById("guest-Form");
 const input = document.getElementById("GuestName");
+const selectCategory = document.querySelectorAll("#guest-category button");  //this is the caategory
 const guestlist = document.getElementById("Guest-List");
-const categorySelect = document.getElementById("Guest-Category"); 
+
 
 let guests = [];
+let selectedCategory = "friend";
 
 // adding a new guest
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  const name = input.value.trim();
-  const category = categorySelect.value;
+//the name place
+  let name = input.value.trim();
+  const category = selectedCategory;
   if (!name) return;
   if (guests.length >= 10) {
     alert("Guest list limit reached (10 guests)");
     return;
   }
 
-  const guest = {
+  let guest = {
     id: Date.now(),
     name,
     attending: true,
@@ -29,6 +31,15 @@ form.addEventListener("submit", (e) => {
   renderGuests();
 });
 
+
+//selected category
+ selectCategory.forEach((btn) => {
+    btn.addEventListener("click", () => {
+    selectedCategory = btn.getAttribute("data-category");
+ });
+ });
+
+// the list of the guest in the event 
 function renderGuests() {
   guestlist.innerHTML = "";
 
@@ -41,7 +52,7 @@ function renderGuests() {
     span.className = guest.attending ? "Attending" : "Not Attending";
 
     const toggleButton = document.createElement("button");
-    toggleButton.textContent = "RSVP";
+    toggleButton.textContent = guest.attending ? "Mark Not Attending": "Mark Attending";
     toggleButton.onclick = () => {
       guest.attending = !guest.attending;
       renderGuests();
